a65be0cc397e993a70239ceb01e0e9b3ea282489
AnjaliNmadev <addystories7@@gmail.com>
2025-04-25 18:33:44 +0530

    sec commit

--- src/component/footer.js
+++ src/component/footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
             <div className="col-sm p-4">
               <h5>Quick Links</h5>
               <ul className="list-unstyled">
-              <li><a href="" className="text-light text-decoration-none">Home</a></li>
-                <li><a href="" className="text-light text-decoration-none">Contact Us</a></li>
-                <li><a href="" className="text-light text-decoration-none">Privacy Policy</a></li>
-                <li><a href="" className="text-light text-decoration-none">About us</a></li>
+              <li><a href="/#" className="text-light text-decoration-none">Home</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Contact Us</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Privacy Policy</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">About us</a></li>
               </ul>
             </div>
   
@@ -19,9 +19,9 @@ const Footer = () => {
             <div className="col-sm p-4">
               <h5> Market Trends</h5>
               <ul className="list-unstyled">
-                <li><a href="" className="text-light text-decoration-none">Bull Market</a></li>
-                <li><a href="" className="text-light text-decoration-none">Bear Market</a></li>
-                <li><a href="" className="text-light text-decoration-none">Sideways Market</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Bull Market</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Bear Market</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Sideways Market</a></li>
               </ul>
             </div>
   
@@ -29,9 +29,9 @@ const Footer = () => {
             <div className="col-sm p-4">
               <h5>Market Types</h5>
               <ul className="list-unstyled">
-                <li><a href="" className="text-light text-decoration-none">Spot Market</a></li>
-                <li><a href="" className="text-light text-decoration-none">Futures Market</a></li>
-                <li><a href="" className="text-light text-decoration-none">Derivatives Market</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Spot Market</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Futures Market</a></li>
+                <li><a href="/#" className="text-light text-decoration-none">Derivatives Market</a></li>
               </ul>
             </div>
   
--- src/component/header.js
+++ src/component/header.js
@@ -5,14 +5,14 @@ function Header()
     <>
      <nav class="navbar navbar-expand-sm fixed-top navbar-dark bg-dark ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#home"><img src={logo} alt="logo" width="80px"/> CryptoPulse </a>
+    <a class="navbar-brand" href="/#"><img src={logo} alt="logo" width="80px"/> CryptoPulse </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active"  href="index.html" >Home</a>
+          <a class="nav-link active"  href="/#" >Home</a>
         </li>
   
         <li class="nav-item dropdown">
@@ -20,9 +20,9 @@ function Header()
           Market Trends
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/">Bull Market</a></li>
-            <li><a class="dropdown-item" href="/" >Bear Market</a></li>
-            <li><a class="dropdown-item" href="/" >Sideways Market</a></li>
+            <li><a class="dropdown-item" href="/#">Bull Market</a></li>
+            <li><a class="dropdown-item" href="/#" >Bear Market</a></li>
+            <li><a class="dropdown-item" href="/#" >Sideways Market</a></li>
             
             
           </ul>
@@ -32,9 +32,9 @@ function Header()
           Market Types
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/">Spot Market</a></li>
-            <li><a class="dropdown-item" href="/" >Futures Market</a></li>
-            <li><a class="dropdown-item" href="/" >Derivatives Market</a></li>
+            <li><a class="dropdown-item" href="/#">Spot Market</a></li>
+            <li><a class="dropdown-item" href="/#" >Futures Market</a></li>
+            <li><a class="dropdown-item" href="/#" >Derivatives Market</a></li>
             
             
           </ul>
